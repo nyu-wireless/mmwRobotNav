6fd8cadf4ad8c8499a63f0490d7be705e4912941
MINGSHENG YIN <49216072+MingshengYin@users.noreply.github.com>
2023-07-30 12:25:59 -0700

    Add files via upload

---
+++ navigation/data/download.py
@@ -0,0 +1,60 @@
+import sys
+import requests
+
+
+def download_file_from_google_drive(id, destination):
+    URL = "https://docs.google.com/uc?export=download&confirm=1"
+
+    session = requests.Session()
+
+    response = session.get(URL, params={"id": id}, stream=True)
+    token = get_confirm_token(response)
+
+    if token:
+        params = {"id": id, "confirm": token}
+        response = session.get(URL, params=params, stream=True)
+
+    save_response_content(response, destination)
+
+
+def get_confirm_token(response):
+    for key, value in response.cookies.items():
+        if key.startswith("download_warning"):
+            return value
+
+    return None
+
+
+def save_response_content(response, destination):
+    CHUNK_SIZE = 32768
+
+    with open(destination, "wb") as f:
+        for chunk in response.iter_content(CHUNK_SIZE):
+            if chunk:  # filter out keep-alive new chunks
+                f.write(chunk)
+
+
+def main():
+    if len(sys.argv) == 2:
+        opt = sys.argv[1]
+    else:
+        print('Cannot download, please check your cammond. *')
+        print('Please run code by python download.py All')
+        exit()
+        
+    drive_id = ['1dcTSEleQoOqriYE0-Hq-r0lHNsQXBSHg', # all.zip
+                '1LE-UTlnt8i9mz6-91D_P3DIUoKO9xxVy'] # tx_posistion.zip
+    print("dowload TxPosition, CHECK README.md to use the coordinates of TX.")
+    size_list = ['6MB']
+    download_file_from_google_drive(drive_id[1], './tx_position.zip')
+    if opt == 'All':
+        print(f"dowload {opt} with {size_list[0]}")
+        download_file_from_google_drive(drive_id[0], './all.zip')
+    else:
+        print('Cannot download, please check your cammond.')
+        print('Please run code by python download.py All')
+    print('done')
+
+
+if __name__ == "__main__":
+    main()
