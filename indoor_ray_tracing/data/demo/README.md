ef4ab07e86e9a4b04457b25a67ae163bae8b9c95
Yaqi Hu <49216072+MingshengYin@users.noreply.github.com>
2023-08-01 21:35:23 -0700

    Update download, move demo

---
+++ indoor_ray_tracing/data/demo/README.md
@@ -0,0 +1,38 @@
+# Ray Tracing Data Set Demo
+
+Plot data from the ray tracing data set.
+
+<p align="center">
+  <img src="https://github.com/nyu-wireless/mmwRobotNav/blob/main/figs/ray_tracing_strongest_aoa.png" width="500">
+</p>
+<p align = "center">
+Fig. 1: The strongest path's AoA
+</p>
+
+<p align="center">
+  <img src="https://github.com/nyu-wireless/mmwRobotNav/blob/main/figs/ray_tracing_strongest_pl.png" width="500">
+</p>
+<p align = "center">
+Fig. 1: The strongest path's path loss
+</p>
+
+<p align="center">
+  <img src="https://github.com/nyu-wireless/mmwRobotNav/blob/main/figs/ray_tracing_strongest_power.png" width="500">
+</p>
+<p align = "center">
+Fig. 1: The strongest path's received power
+</p>
+
+<p align="center">
+  <img src="https://github.com/nyu-wireless/mmwRobotNav/blob/main/figs/ray_tracing_second_aoa.png" width="500">
+</p>
+<p align = "center">
+Fig. 1: The second strongest path's AoA 
+</p>
+
+<p align="center">
+  <img src="https://github.com/nyu-wireless/mmwRobotNav/blob/main/figs/ray_tracing_second_power.png" width="500">
+</p>
+<p align = "center">
+Fig. 1: The second strongest path's received power
+</p>
--- indoor_ray_tracing/data/download.py
+++ indoor_ray_tracing/data/download.py
@@ -63,83 +63,51 @@ def unzip_file(zip_file_path, extract_to):
 
 
 def main():
-    if len(sys.argv) == 2:
-        opt = sys.argv[1]
-    else:
-        print('Cannot download, please check your cammond. *')
-        print('Please run code by python download.py PartX')
+    if len(sys.argv) != 2:
+        print('Cannot download, please check your command.')
+        print('Please run the code by python download.py PartX')
         print('Replace PartX with one of: Part1, Part2, Part3, Part4, Part5, All')
         exit()
-        
-    drive_id = ['1lb1mAEFVloVSyQ_ObXXnE7WwZLdS7aDn', # Part1.zip
-                '10jss0_u6z_Va7DwKFtPXcwGNBabYoYD5', # Part2.zip
-                '1_I9FEuH8YIOUDZt_FRUZeFE5ph8ICf7P', # Part3.zip
-                '1Ia1-XyiW3azMxCXVEXeZfscFaMyyBMzi', # Part4.zip
-                '18ZvOPpAx8x7jqLC8Org7Umkx0ZsD7mDr', # Part5.zip
-                '1LE-UTlnt8i9mz6-91D_P3DIUoKO9xxVy'] # tx_posistion.zip
-    print("dowload TxPosition, CHECK README.md to use the coordinates of TX.")
-    size_list = ['74MB', "88MB", '103MB', '80MB', '66MB']
+
+    opt = sys.argv[1]
+    drive_id = [
+        '1lb1mAEFVloVSyQ_ObXXnE7WwZLdS7aDn',  # Part1.zip
+        '10jss0_u6z_Va7DwKFtPXcwGNBabYoYD5',  # Part2.zip
+        '1_I9FEuH8YIOUDZt_FRUZeFE5ph8ICf7P',  # Part3.zip
+        '1Ia1-XyiW3azMxCXVEXeZfscFaMyyBMzi',  # Part4.zip
+        '18ZvOPpAx8x7jqLC8Org7Umkx0ZsD7mDr',  # Part5.zip
+        '1LE-UTlnt8i9mz6-91D_P3DIUoKO9xxVy'   # tx_position.zip
+    ]
+    size_list = ['74MB', '88MB', '103MB', '80MB', '66MB']
+
+    print("Download TxPosition, CHECK README.md to use the coordinates of TX.")
     download_file_from_google_drive(drive_id[5], './tx_position.zip')
-    if opt == 'Part1':
-        print(f"dowload {opt} with {size_list[0]}")
-        download_file_from_google_drive(drive_id[0], './Part1.zip')
-    elif opt == 'Part2':
-        print(f"dowload {opt} with {size_list[1]}")
-        download_file_from_google_drive(drive_id[1], './Part2.zip')
-    elif opt == 'Part3':
-        print(f"dowload {opt} with {size_list[2]}")
-        download_file_from_google_drive(drive_id[2], './Part3.zip')
-    elif opt == 'Part4':
-        print(f"dowload {opt} with {size_list[3]}")
-        download_file_from_google_drive(drive_id[3], './Part4.zip')
-    elif opt == 'Part5':
-        print(f"dowload {opt} with {size_list[4]}")
-        download_file_from_google_drive(drive_id[4], './Part5.zip')
-    elif opt == 'All':
-        print(f"dowload {opt} with {size_list[0]}")
-        download_file_from_google_drive(drive_id[0], './Part1.zip')
-        print(f"dowload {opt} with {size_list[1]}")
-        download_file_from_google_drive(drive_id[1], './Part2.zip')
-        print(f"dowload {opt} with {size_list[2]}")
-        download_file_from_google_drive(drive_id[2], './Part3.zip')
-        print(f"dowload {opt} with {size_list[3]}")
-        download_file_from_google_drive(drive_id[3], './Part4.zip')
-        print(f"dowload {opt} with {size_list[4]}")
-        download_file_from_google_drive(drive_id[4], './Part5.zip')
+
+    if opt == 'All':
+        for i in range(5):
+            print(f"Download Part{i + 1} with {size_list[i]}")
+            download_file_from_google_drive(drive_id[i], f'./Part{i + 1}.zip')
+    elif opt.startswith('Part') and opt[4:].isdigit() and 1 <= int(opt[4:]) <= 5:
+        part_num = int(opt[4:])
+        print(f"Download {opt} with {size_list[part_num - 1]}")
+        download_file_from_google_drive(drive_id[part_num - 1], f'./{opt}.zip')
     else:
-        print('Cannot download, please check your cammond.')
-        print('Please run code by python download.py PartX')
+        print('Invalid command. Please run the code by python download.py PartX')
         print('Replace PartX with one of: Part1, Part2, Part3, Part4, Part5, All')
         exit()
-    print('done')
 
-    print('start unzip')
+    print('Download completed. Starting unzip.')
 
-    # After downloading the files, unzip them if needed
-    if opt == 'Part1':
+    if opt == 'All':
+        for i in range(5):
+            print(f"Unzipping Part{i + 1}.zip...")
+            unzip_file(f'./Part{i + 1}.zip', './')
+    else:
         print(f"Unzipping {opt}.zip...")
-        unzip_file('./Part1.zip', './')
-    elif opt == 'Part2':
-        print(f"Unzipping {opt}.zip...")
-        unzip_file('./Part2.zip', './')
-    elif opt == 'Part3':
-        print(f"Unzipping {opt}.zip...")
-        unzip_file('./Part3.zip', './')
-    elif opt == 'Part4':
-        print(f"Unzipping {opt}.zip...")
-        unzip_file('./Part4.zip', './')
-    elif opt == 'Part5':
-        print(f"Unzipping {opt}.zip...")
-        unzip_file('./Part5.zip', './')
-    elif opt == 'All':
-        print(f"Unzipping {opt}.zip...")
-        unzip_file('./Part1.zip', './')
-        unzip_file('./Part2.zip', './')
-        unzip_file('./Part3.zip', './')
-        unzip_file('./Part4.zip', './')
-        unzip_file('./Part5.zip', './')
+        unzip_file(f'./{opt}.zip', './')
+
     unzip_file('./tx_position.zip', './')
-    print('done')
+    print('Unzip completed.')
 
 if __name__ == "__main__":
     main()
